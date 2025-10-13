ca1576b4aa780f6b3c0babc13cd62e96ef95fd2c
Larissa Faria <lari.f4ria@gmail.com>
2025-10-13 20:50:28 -0300

    add mock users data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (name, created_at) VALUES
+('Vanessa Forin', '2025-10-10 14:30:00'),
+('Lucas Andrade', '2025-10-11 09:45:00'),
+('Marina Souza', '2025-10-12 16:10:00'),
+('João Pereira', '2025-10-13 08:00:00'),
+('Carla Menezes', '2025-10-13 10:25:00');
