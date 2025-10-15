0ab51d1b78d9ec97e09d46c45571696ff60d2285
Thais Machado <thais.machado+lopti@lopti.ai>
2025-10-15 11:36:58 -0300

    fixing paths

--- src/main/resources/static/js/dashboard.js
+++ src/main/resources/static/js/dashboard.js
@@ -136,7 +136,7 @@ const cards = [
       row.addEventListener('click', () => {
         const id = row.dataset.id;
         localStorage.setItem('cardsMock', JSON.stringify(cards));
-        window.location.href = `edit-cards.html?id=${id}`;
+        window.location.href = `edit-card.html?id=${id}`;
       });
     });
   
